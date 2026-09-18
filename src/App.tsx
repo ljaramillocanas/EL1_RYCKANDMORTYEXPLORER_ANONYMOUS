e16947190e39ae634e3a52b62d2a3040b7858c35
JulianKZz <julianda206@hotmail.com>
2026-09-18 02:02:12 -0500

    feat: add debounced character filtering

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ function App() {
   const [error, setError] = useState<string | null>(null)
   const [visibleCount, setVisibleCount] = useState(0)
   const [searchText, setSearchText] = useState<string>("")
+  const [debouncedSearchText, setDebouncedSearchText] = useState<string>("")
+
 
   function descubrirPersonajes() {
     if (visibleCount < characters.length) {
@@ -44,6 +46,20 @@ function App() {
 
   const visibleCharacters = characters.slice(0, visibleCount)
 
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setDebouncedSearchText(searchText)
+    }, 400)
+
+    return () => clearTimeout(timeoutId)
+  }, [searchText])
+
+  const filteredCharacters = visibleCharacters.filter((character) =>
+  character.name
+    .toLowerCase()
+    .includes(debouncedSearchText.toLowerCase())
+)
+
   return (
     <>
       <h1>Rick & Morty Explorer</h1>
@@ -74,7 +90,7 @@ function App() {
         ) : characters.length === 0 ? (
           <p>No se encontraron personajes.</p>
         ) : (
-          visibleCharacters.map((character) => (
+          filteredCharacters.map((character) => (
             <CharacterCard
               image={character.image}
               id={character.id}
